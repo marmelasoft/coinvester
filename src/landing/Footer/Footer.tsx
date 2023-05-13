4d476631d0f30902724e2912df6d4e2cad8f17d3
Nelson Estevão <nelson@marmelasoft.com>
2023-05-13 19:38:17 +0100

    feat: update footer information (#12)

--- src/landing/Footer/Footer.tsx
+++ src/landing/Footer/Footer.tsx
@@ -31,7 +31,7 @@ const products: ProductType[] = [
     }
 ]
 
-const footer = () => {
+const Footer = () => {
     return (
         <div className="bg-black -mt-40" id="first-section ">
             <div className="mx-auto max-w-2xl pt-48 pb-16 px-4 sm:px-6 lg:max-w-7xl lg:px-8">
@@ -40,7 +40,7 @@ const footer = () => {
                     {/* COLUMN-1 */}
 
                     <div className='col-span-4'>
-                        <h3 className='text-white text-4xl font-semibold leading-9 mb-4 lg:mb-20'> Desgy Solutions</h3>
+                        <h3 className='text-white text-4xl font-semibold leading-9 mb-4 lg:mb-20'>coinvest</h3>
                         <div className='flex gap-4'>
                             <div className='footer-icons'>
                                 <Link href="https://facebook.com"><Image src={'/images/footer/vec.svg'} alt="facebook" width={15} height={20} /></Link>
@@ -78,7 +78,7 @@ const footer = () => {
                 <div className="pt-5 pb-5 px-4 sm:px-6 lg:px-4 border-solid border-t border-footer">
                     <div className="mt-4 grid grid-cols-1 gap-y-10 gap-x-16 sm:grid-cols-2 xl:gap-x-8">
                         <div>
-                            <h3 className='text-center md:text-start text-offwhite text-lg'>@2023 - All Rights Reserved by <Link href="https://adminmart.com/" target="_blank"> Adminmart.com</Link></h3>
+                            <h3 className='text-left md:text-start text-offwhite text-lg'>@2023 - All Rights Reserved by <Link href="https://marmelasoft.com" target="_blank">Marmelasoft, Inc.</Link></h3>
                         </div>
                         <div className="flex justify-center md:justify-end">
                             <Link href="/">
@@ -97,4 +97,4 @@ const footer = () => {
     )
 }
 
-export default footer;
+export default Footer;
