d2eb8ba4d96cf88a350b538a5998c6fbc372eecb
Nelson Estevão <nelson@marmelasoft.com>
2023-05-13 16:58:32 +0100

    chore: refactor landing components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,6 @@
 import Head from "next/head";
-import Navbar from "../components/Navbar/index";
-import Banner from "../components/Banner/index";
-import Aboutus from "../components/Aboutus/index";
-import Dedicated from "../components/Dedicated/index";
-import Digital from "../components/Digital/index";
-import Beliefs from "../components/Beliefs/index";
-import Wework from "../components/Wework/index";
-import Ourteam from "../components/Ourteam/index";
-import Manage from "../components/Manage/index";
-import FAQ from "../components/FAQ/index";
-import Testimonials from "../components/Testimonials/index";
-import Articles from "../components/Articles/index";
-import Joinus from "../components/Joinus/index";
-import Insta from "../components/Insta/index";
-import Footer from "../components/Footer/index";
+
+import { Navbar, Banner, Aboutus, Digital, Dedicated, Manage, FAQ, Footer, Wework } from "@landing";
 
 const Home = () => {
   return (
