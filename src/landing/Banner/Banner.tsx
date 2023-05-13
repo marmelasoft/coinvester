c6f814ecc054a74fde15a4bfc3bd26c4d6ca2c45
Luis Araújo <l.araujoo.1996@gmail.com>
2023-05-13 20:02:15 +0100

    feat: add button components

--- src/landing/Banner/Banner.tsx
+++ src/landing/Banner/Banner.tsx
@@ -1,36 +1,37 @@
+import { Button } from "@/components/Button";
 import Image from "next/image";
 
 const Banner = () => {
-    return (
-        <div className='mx-auto max-w-7xl my-10 sm:py-10 px-6 lg:px-8'>
-            <div className='grid grid-cols-1 lg:grid-cols-2 my-16'>
+  return (
+    <div className="px-6 mx-auto my-10 max-w-7xl sm:py-10 lg:px-8">
+      <div className="grid grid-cols-1 my-16 lg:grid-cols-2">
+        {/* COLUMN-1 */}
 
-                {/* COLUMN-1 */}
-
-                <div className="mx-auto sm:mx-0">
-                    <div className='py-8 text-center lg:text-start'>
-                    </div>
-                    <div className="py-3 text-center lg:text-start">
-                        <h1 className='text-6xl lg:text-80xl font-bold text-darkpurple'>
-                            Democratizing access to investment
-                        </h1>
-                    </div>
-                    <div className='my-7 text-center lg:text-start'>
-                        <button className='text-sm md:text-xl font-semibold hover:shadow-xl bg-blue text-white py-3 px-6 md:py-5 md:px-14 rounded-full hover:bg-hoblue'>
-                            Get Started
-                        </button>
-                    </div>
-                </div>
-
-                {/* COLUMN-2 */}
-
-                <div className='hidden lg:block'>
-                    <Image src="/images/banner/banner.svg" alt="hero-image" width={800} height={642} />
-                </div>
-
-            </div>
+        <div className="mx-auto sm:mx-0">
+          <div className="py-8 text-center lg:text-start"></div>
+          <div className="py-3 text-center lg:text-start">
+            <h1 className="text-6xl font-bold lg:text-80xl text-darkpurple">
+              Democratizing access to investment
+            </h1>
+          </div>
+          <div className="text-sm font-semibold text-center md:text-xl my-7 lg:text-start">
+            <Button>Start Investing</Button>
+          </div>
         </div>
-    )
-}
+
+        {/* COLUMN-2 */}
+
+        <div className="hidden lg:block">
+          <Image
+            src="/images/banner/banner.svg"
+            alt="hero-image"
+            width={800}
+            height={642}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Banner;
