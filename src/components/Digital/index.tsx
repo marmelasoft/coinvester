3940ee9b962f5a10a3ab9b1028211d140fae3c4f
Nelson Estevão <nelson@marmelasoft.com>
2023-05-13 16:26:30 +0100

    Update src/components/Digital/index.tsx

--- src/components/Digital/index.tsx
+++ src/components/Digital/index.tsx
@@ -11,7 +11,8 @@ const Digital = () => {
 
                     <div className="pt-24 lg:pl-24 ">
                         <h3 className="text-lg font-normal text-white mb-5 tracking-widest text-center lg:text-start">WHO WE ARE</h3>
-                        <h4 className="text-4xl sm:text-6xl font-bold text-white mb-8 leading-snug text-center lg:text-start">A Real Estate market who facilitates investments.</h4>
+                        <h4 className="text-4xl sm:text-6xl font-bold text-white mb-8 leading-snug text-center lg:text-start">A Real Estate market that facilitates investments.</h4>
+
                         <div className="text-center lg:text-start">
                             <button className="text-xl font-semibold text-white bg-btnblue py-4 px-12 hover:bg-hoblue rounded-full">Get started</button>
                         </div>
