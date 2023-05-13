065b1977f8c9752dade43b1e0e04e32a9dcadb03
Nelson Estevão <nelson@marmelasoft.com>
2023-05-13 16:25:07 +0100

    Update src/components/Dedicated/index.tsx

--- src/components/Dedicated/index.tsx
+++ src/components/Dedicated/index.tsx
@@ -17,7 +17,8 @@ const Dedicated = () => {
                     {/* COLUMN-2 */}
                     <div className="relative">
                         <Image src="images/dedicated/comma.svg" alt="comma-image" width={200} height={106} className="absolute comma-pos hidden lg:block" />
-                        <h2 className="text-4xl lg:text-65xl pt-4 font-bold sm:leading-tight mt-5 text-center lg:text-start">Dedicated to share whealth amoungst everyone</h2>
+                        <h2 className="text-4xl lg:text-65xl pt-4 font-bold sm:leading-tight mt-5 text-center lg:text-start">Dedicated to share wealth amongst everyone</h2>
+
                         <p className="font-medium text-lightblack text-2xl mt-5 text-center lg:text-start">We believe that real estate growth should serve the needs of the entire population. That's why we're committed to ensuring that everyone has a voice in the process. No one should be left out when it comes to shaping the communities where we live, work, and play. Join us in creating a more inclusive and equitable future for all.</p>
                         <p className="text-2xl font-semibold mt-12 lg:ml-32 text-center lg:text-start"> Nelson Estevão, CEO</p>
                     </div>
