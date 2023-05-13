331e25128e58243381d8e7dcb0cb429eeba53882
Luis Araújo <44007026+laraujo7@users.noreply.github.com>
2023-05-13 12:39:21 +0100

    feat: add images (#8)
    * feat: add landing template
    
    * feat: add images

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+
+const Banner = () => {
+    return (
+        <div className='mx-auto max-w-7xl my-10 sm:py-10 px-6 lg:px-8'>
+            <div className='grid grid-cols-1 lg:grid-cols-2 my-16'>
+
+                {/* COLUMN-1 */}
+
+                <div className="mx-auto sm:mx-0">
+                    <div className='py-3 text-center lg:text-start'>
+                        <button className='text-blue bg-lightblue hover:shadow-xl text-sm md:text-lg font-bold px-6 py-1 rounded-3xl tracking-wider hover:text-white hover:bg-black'>DESIGN AGENCY</button>
+                    </div>
+                    <div className="py-3 text-center lg:text-start">
+                        <h1 className='text-6xl lg:text-80xl font-bold text-darkpurple'>
+                            Dedicated to <br /> bring your <br /> ideas to life.
+                        </h1>
+                    </div>
+                    <div className='my-7 text-center lg:text-start'>
+                        <button className='text-sm md:text-xl font-semibold hover:shadow-xl bg-blue text-white py-3 px-6 md:py-5 md:px-14 rounded-full hover:bg-hoblue'>
+                            Get Started
+                        </button>
+                    </div>
+                </div>
+
+                {/* COLUMN-2 */}
+
+                <div className='lg:-m-24 lg:pt-20 hidden lg:block'>
+                    <Image src="/images/banner/banner.svg" alt="hero-image" width={800} height={642} />
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Banner;
