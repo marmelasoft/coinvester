a36843fd58737aee1ba766054d6516e93b46f1eb
Francisco Lira <arjot12@gmail.com>
2023-05-13 16:22:01 +0100

    add content to landing

--- src/components/Wework/index.tsx
+++ src/components/Wework/index.tsx
@@ -12,33 +12,33 @@ interface DataType {
 
 const postData: DataType[] = [
     {
-        profession: 'Co-founder',
-        name: 'John Doe',
+        profession: 'Braga',
+        name: '2 Bedroom House ',
         imgSrc: '/images/wework/avatar.svg',
     },
     {
-        profession: 'Co-founder',
-        name: 'John Doe',
+        profession: 'Porto',
+        name: '3 Bedroom Apt.',
         imgSrc: '/images/wework/avatar3.svg',
     },
     {
-        profession: 'Co-founder',
-        name: 'John Doe',
+        profession: 'Braga',
+        name: '4 Bedroom House',
         imgSrc: '/images/wework/avatar4.svg',
     },
     {
-        profession: 'Co-founder',
-        name: 'John Doe',
+        profession: 'Guimarães',
+        name: '1 Bedroom Apartment',
         imgSrc: '/images/wework/avatar.svg',
     },
     {
-        profession: 'Co-founder',
-        name: 'John Doe',
+        profession: 'Famalicão',
+        name: '2 Bedroom Apartment',
         imgSrc: '/images/wework/avatar3.svg',
     },
     {
-        profession: 'Co-founder',
-        name: 'John Doe',
+        profession: 'Braga',
+        name: '6 Bedroom Apartment',
         imgSrc: '/images/wework/avatar4.svg',
     },
 ]
@@ -98,9 +98,9 @@ export default class MultipleItems extends Component {
                 <div className='mx-auto max-w-2xl lg:max-w-7xl sm:py-4 lg:px-8 '>
 
                     <div className="text-center">
-                        <h3 className="text-4xl sm:text-6xl font-bold text-black my-2">We work in several verticals.</h3>
-                        <h3 className="text-4xl sm:text-6xl font-bold text-black opacity-50 lg:mr-48 my-2">We work in several verticals.</h3>
-                        <h3 className="text-4xl sm:text-6xl font-bold text-black opacity-25 lg:-mr-48 my-2">We work in several verticals.</h3>
+                        <h3 className="text-4xl sm:text-6xl font-bold text-black my-2">Different type of investments.</h3>
+                        <h3 className="text-4xl sm:text-6xl font-bold text-black opacity-50 lg:mr-48 my-2">Different type of investments.</h3>
+                        <h3 className="text-4xl sm:text-6xl font-bold text-black opacity-25 lg:-mr-48 my-2">Different type of investments.</h3>
                     </div>
 
                 </div>
@@ -111,7 +111,6 @@ export default class MultipleItems extends Component {
                             <div className='bg-white m-3 py-14 my-10 text-center shadow-xl rounded-3xl'>
                                 <div className='relative'>
                                     <Image src={items.imgSrc} alt="gaby" width={182} height={182} className="inline-block m-auto" />
-                                    <Image src={'/images/wework/linkedin.svg'} alt="greenbg" width={120} height={120} className=" absolute inline-block position-linkedin" />
                                 </div>
                                 <h4 className='text-4xl font-bold pt-14'>{items.name}</h4>
                                 <h3 className='text-2xl font-normal pt-4 pb-2 opacity-50'>{items.profession}</h3>
