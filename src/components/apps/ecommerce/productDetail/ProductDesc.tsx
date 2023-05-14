c26e034bbb1a0470ebb32e7095286397d9599a41
Luis Araújo <44007026+laraujo7@users.noreply.github.com>
2023-05-14 04:54:55 +0100

    feat: improve product design (#21)

--- src/components/apps/ecommerce/productDetail/ProductDesc.tsx
+++ src/components/apps/ecommerce/productDetail/ProductDesc.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   Box,
   Typography,
@@ -10,9 +10,9 @@ import {
   Rating,
   Button,
   Paper,
-} from '@mui/material';
-import { IconPencil } from '@tabler/icons-react';
-import ChildCard from '../../../../components/shared/ChildCard';
+} from "@mui/material";
+import { IconPencil } from "@tabler/icons-react";
+import ChildCard from "../../../../components/shared/ChildCard";
 
 interface ProductCardProps {
   like: number;
@@ -29,12 +29,19 @@ interface TabProps {
 // progress
 function ProgressBar({ like, star, value, ...others }: ProductCardProps) {
   return (
-    <Box display={'flex'} alignItems="center" gap="20px">
+    <Box display={"flex"} alignItems="center" gap="20px">
       <Box sx={{ minWidth: 50 }}>
-        <Typography variant="body2" color="textSecondary">{`${Math.round(star)} Stars`}</Typography>
+        <Typography variant="body2" color="textSecondary">{`${Math.round(
+          star
+        )} Stars`}</Typography>
       </Box>
-      <Box sx={{ width: '100%' }}>
-        <LinearProgress value={value} variant="determinate" color="primary" {...others} />
+      <Box sx={{ width: "100%" }}>
+        <LinearProgress
+          value={value}
+          variant="determinate"
+          color="primary"
+          {...others}
+        />
       </Box>
       <Box sx={{ minWidth: 35 }}>
         <Typography variant="subtitle2">{`(${Math.round(like)})`}</Typography>
@@ -62,7 +69,7 @@ const TabPanel = (props: TabProps) => {
 const a11yProps = (index: number) => {
   return {
     id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
   };
 };
 
@@ -76,7 +83,7 @@ const ProductDesc = () => {
   return (
     <ChildCard>
       <Box>
-        <Box sx={{ borderBottom: 1, borderColor: 'grey.100' }}>
+        <Box sx={{ borderBottom: 1, borderColor: "grey.100" }}>
           <Tabs
             value={value}
             onChange={handleChange}
@@ -94,20 +101,28 @@ const ProductDesc = () => {
         {/* Decription */}
         {/* ------------------------------------------- */}
         <TabPanel value={value} index={0}>
-          <Typography variant="h5">
-            Sed at diam elit. Vivamus tortor odio, pellentesque eu tincidunt a, aliquet sit amet
-            lorem pellentesque eu tincidunt a, aliquet sit amet lorem.
+          <Typography variant="h4">
+            Sed at diam elit. Vivamus tortor odio, pellentesque eu tincidunt a,
+            aliquet sit amet lorem pellentesque eu tincidunt a, aliquet sit amet
+            lorem.
           </Typography>
-          <Typography color="textSecondary" mt={4}>
-            Cras eget elit semper, congue sapien id, pellentesque diam. Nulla faucibus diam nec
-            fermentum ullamcorper. Praesent sed ipsum ut augue vestibulum malesuada. Duis vitae
-            volutpat odio. Integer sit amet elit ac justo sagittis dignissim.
+          <Typography variant="h5" color="textSecondary" mt={4}>
+            Cras eget elit semper, congue sapien id, pellentesque diam. Nulla
+            faucibus diam nec fermentum ullamcorper. Praesent sed ipsum ut augue
+            vestibulum malesuada. Duis vitae volutpat odio. Integer sit amet
+            elit ac justo sagittis dignissim.
           </Typography>
-          <Typography color="textSecondary" variant="body1" fontWeight={400} mt={4}>
-            Vivamus quis metus in nunc semper efficitur eget vitae diam. Proin justo diam, venenatis
-            sit amet eros in, iaculis auctor magna. Pellentesque sit amet accumsan urna, sit amet
-            pretium ipsum. Fusce condimentum venenatis mauris et luctus. Vestibulum ante ipsum
-            primis in faucibus orci luctus et ultrices posuere cubilia curae;
+          <Typography
+            color="textSecondary"
+            variant="h5"
+            fontWeight={400}
+            mt={4}
+          >
+            Vivamus quis metus in nunc semper efficitur eget vitae diam. Proin
+            justo diam, venenatis sit amet eros in, iaculis auctor magna.
+            Pellentesque sit amet accumsan urna, sit amet pretium ipsum. Fusce
+            condimentum venenatis mauris et luctus. Vestibulum ante ipsum primis
+            in faucibus orci luctus et ultrices posuere cubilia curae;
           </Typography>
         </TabPanel>
         {/* ------------------------------------------- */}
@@ -119,12 +134,12 @@ const ProductDesc = () => {
             {/* Average Rate Tab */}
             {/* ------------------------------------------- */}
             <Grid item xs={12} lg={4}>
-              <Paper variant="outlined" sx={{ height: '100%', p: 3 }}>
+              <Paper variant="outlined" sx={{ height: "100%", p: 3 }}>
                 <Stack
                   alignItems="center"
                   justifyContent="center"
                   spacing={2}
-                  sx={{ height: '100%' }}
+                  sx={{ height: "100%" }}
                 >
                   <Typography variant="subtitle1">Average Rating</Typography>
                   <Typography variant="h1" color="primary" fontWeight={600}>
@@ -139,7 +154,12 @@ const ProductDesc = () => {
             {/* ------------------------------------------- */}
             <Grid item xs={12} lg={4}>
               <Paper variant="outlined" sx={{ p: 3 }}>
-                <Grid container alignItems="center" justifyContent="space-between" spacing={2}>
+                <Grid
+                  container
+                  alignItems="center"
+                  justifyContent="space-between"
+                  spacing={2}
+                >
                   <Grid item xs={12}>
                     <ProgressBar star={1} value={45} like={485} />
                   </Grid>
@@ -162,14 +182,18 @@ const ProductDesc = () => {
             {/* Button */}
             {/* ------------------------------------------- */}
             <Grid item xs={12} lg={4}>
-              <Paper sx={{ height: '100%', p: 3 }} variant="outlined">
+              <Paper sx={{ height: "100%", p: 3 }} variant="outlined">
                 <Stack
                   alignItems="center"
                   justifyContent="center"
                   spacing={2}
-                  sx={{ height: '100%' }}
+                  sx={{ height: "100%" }}
                 >
-                  <Button variant="outlined" size="large" startIcon={<IconPencil />}>
+                  <Button
+                    variant="outlined"
+                    size="large"
+                    startIcon={<IconPencil />}
+                  >
                     Write an Review
                   </Button>
                 </Stack>
